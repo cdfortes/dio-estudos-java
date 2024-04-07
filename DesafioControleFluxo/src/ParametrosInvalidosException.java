20d74e80811badef1ff0e27b3e5123854e6ee838
cdfortes <cdfortes@gmail.com>
2024-04-06 23:20:55 -0100

    controle de fluxo

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,18 @@
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+    public ParametrosInvalidosException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public ParametrosInvalidosException(Throwable cause) {
+        super(cause);
+    }
+
+    public ParametrosInvalidosException() {
+        super();
+    }
+}
