171249c802bd6ba4129fc93d83c3c8dbf7e22712
cdfortes <cdfortes@gmail.com>
2024-04-10 12:44:58 -0100

    Lab LojaPOO - estrutura de pastas e classes

---
+++ LojaLivrosPOO/src/dio/cdfortes/entidade/contantes/Genero.java
@@ -0,0 +1,25 @@
+package dio.cdfortes.entidade.contantes;
+
+public enum Genero {
+    DRAMA(15),
+
+    SUSPENSE(10),
+
+    ROMANCE(5);
+
+    private double fator;
+
+    /**
+     * Construtor.
+     * 
+     * @param fator Valor por tipo que influencia no cálculo do frete.
+     */
+    Genero(double fator) {
+        this.fator = fator / 100;
+    }
+
+    public double getFator() {
+        return fator;
+    }
+
+}
